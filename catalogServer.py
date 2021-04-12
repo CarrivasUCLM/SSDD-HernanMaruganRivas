a98102810cb2a51399a037b073d77ec342a62dc6
cristinamarugan <cristina.marugan@alu.uclm.es>
2021-04-12 20:14:52 +0200

    Comienzo canal de eventos ServiceAvailability

--- catalogServer.py
+++ catalogServer.py
@@ -1,2 +1,48 @@
 #!/usr/bin/env python3
 #-*- conding: utf-8 -*-
+
+import sys
+import Ice
+Ice.loadSlice('iceflix.ice')
+
+import IceFlix
+
+class MediaCatalogI(IceFlix.MediaCatalog):
+    def getTile(self, id):
+        return 0
+    
+    def getTilesByName(self, name, exact):
+        return True
+
+    def getTilesByTags(self, tags, includeAllTags):
+        return 0
+
+    def renameTile(self, id, name, authentication):
+        return 0
+
+    def addTags(self, id, tags, authentication):
+        return 0
+    
+    def removeTags(self, id, tags, authentication):
+        return 0
+    
+
+class Server(Ice.Application):
+    def run(self, argv):
+        broker = self.communicator()
+        servant = MediaCatalogI()
+
+        adapter = broker.createObjectAdapter("CatalogAdapter")
+        proxy = adapter.add(servant, broker.stringToIdentity("catalog"))
+
+        print(proxy, flush=True)
+
+        adapter.activate()
+        self.shutdownOnInterrupt()
+        broker.waitForShutdown()
+
+        return 0
+
+
+server = Server()
+sys.exit(server.main(sys.argv))
--- iceFlixServer.py
+++ iceFlixServer.py
@@ -6,6 +6,7 @@ import Ice
 Ice.loadSlice('iceflix.ice')
 
 import IceFlix
+import IceStorm
 
 
 class MainI(IceFlix.Main):
@@ -16,7 +17,17 @@ class MainI(IceFlix.Main):
         return 0
 
 
-class ServiceAvailabilityI(IceFlix.ServiceAvailability):
+class ServiceAvailabilityI(Ice.Application, IceFlix.ServiceAvailability):
+    
+    def get_topic_manager(self):
+        key = 'IceStorm.TopicManager.Proxy'
+        proxy = self.communicator().propertyToProxy(key)
+        if proxy is None:
+            print ("property {} not set".format(key))
+            return None
+        print("Using IceStorm in: '%s'" % key)
+        return IceStorm.TopicManagerPrx.checkedCast(proxy)
+
     def catalogService(self, service, id):
         return 0
 
@@ -26,7 +37,26 @@ class ServiceAvailabilityI(IceFlix.ServiceAvailability):
     def mediaService(self, service, id):
         return 0
     
+    def __init__(self, servant):
+        topic_mgr = self.get_topic_manager()
+        if not topic_mgr:
+            print('Invalid proxy')
+            return 2
+        topic_name = 'ServiceAvailability'
+        try:
+            topic = topic_mgr.retrieve(topic_name)
+        except IceStorm.NoSuchTopic:
+            print ("no such topic found, creating")
+            topic = topic_mgr.create(topic_name)
+        publisher = topic.getPublisher()
+        ServiceAvailability = IceFlix.ServiceAvailabilityPrx.uncheckedCast(publisher)
+        print("Hola")
+        return 0 
+        ####Continuar aqui
+
+    
 class Server(Ice.Application):
+    
     def run(self, argv):
         broker = self.communicator()
         servant = MainI()
