029f0b02e3ad85ced353482114cb3e7a2124f435
CarrivasUclm <Carlos.Rivas1@alu.uclm.es>
2021-05-02 11:14:53 +0200

    common.py added

---
+++ common.py
@@ -0,0 +1,27 @@
+'''
+IceFlix common names
+'''
+
+SERVICE_ANNOUNCEMENT_INTERVAL = 5.0 # seconds
+SERVICE_ANNOUNCEMENTS_TOPIC = 'ServiceAvailability'
+
+MAIN_ADAPTER_NAME = 'IceFlixAdapter'
+MAIN_IDENTITY = 'Identity'
+
+AUTH_ADAPTER_NAME = 'AuthAdapter'
+AUTH_DB_FILE = 'Auth.File'
+AUTH_IDENTITY = 'Identity'
+
+CATALOG_ADAPTER_NAME = 'CatalogAdapter'
+CATALOG_DB_FILE = 'Catalog.File'
+CATALOG_IDENTITY = 'Identity'
+
+MEDIA_FOLDER = 'Media.Folder'
+MEDIA_ADAPTER_NAME = 'MediaAdapter'
+MEDIA_IDENTITY = 'Identity'
+
+AUTH_ANNOUNCEMENTS_TOPIC = 'AuthenticationStatus'
+MEDIA_ANNOUNCEMENTS_TOPIC = 'MediaAnnouncements'
+
+ICESTORM_PROXY_PROPERTY = 'IceStorm.TopicManager.Proxy'
+AUTH_SERVICE_PROXY_PROPERTY = 'AuthService.Proxy'
